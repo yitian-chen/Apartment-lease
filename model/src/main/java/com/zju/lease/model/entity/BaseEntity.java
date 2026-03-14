bf02704f837415ea6eec6d0ec9bee61959c32d07
Chen <chenyitian01@zju.edu.cn>
2026-03-14 15:39:00 +0800

    完成全部支付方式查询方法

--- model/src/main/java/com/zju/lease/model/entity/BaseEntity.java
+++ model/src/main/java/com/zju/lease/model/entity/BaseEntity.java
@@ -3,6 +3,8 @@ package com.zju.lease.model.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
@@ -18,14 +20,18 @@ public class BaseEntity implements Serializable {
 
     @Schema(description = "创建时间")
     @TableField(value = "create_time")
+    @JsonIgnore
     private Date createTime;
 
     @Schema(description = "更新时间")
     @TableField(value = "update_time")
+    @JsonIgnore
     private Date updateTime;
 
     @Schema(description = "逻辑删除")
+    @TableLogic
     @TableField("is_deleted")
+    @JsonIgnore
     private Byte isDeleted;
 
 }
--- web/web-admin/src/main/java/com/zju/lease/web/admin/controller/apartment/PaymentTypeController.java
+++ web/web-admin/src/main/java/com/zju/lease/web/admin/controller/apartment/PaymentTypeController.java
@@ -1,10 +1,13 @@
 package com.zju.lease.web.admin.controller.apartment;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.zju.lease.common.result.Result;
 import com.zju.lease.model.entity.PaymentType;
+import com.zju.lease.web.admin.service.PaymentTypeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,10 +18,14 @@ import java.util.List;
 @RestController
 public class PaymentTypeController {
 
+    @Autowired
+    private PaymentTypeService service;
+
     @Operation(summary = "查询全部支付方式列表")
     @GetMapping("list")
     public Result<List<PaymentType>> listPaymentType() {
-        return Result.ok();
+        List<PaymentType> list = service.list();
+        return Result.ok(list);
     }
 
     @Operation(summary = "保存或更新支付方式")
