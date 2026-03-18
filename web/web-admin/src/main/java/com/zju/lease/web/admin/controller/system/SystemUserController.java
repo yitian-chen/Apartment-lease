6897d841b68480ad11f40b8961b3de8923cf2f39
Chen <chenyitian01@zju.edu.cn>
2026-03-18 08:34:08 +0800

    完成后台用户管理的所有接口

--- web/web-admin/src/main/java/com/zju/lease/web/admin/controller/system/SystemUserController.java
+++ web/web-admin/src/main/java/com/zju/lease/web/admin/controller/system/SystemUserController.java
@@ -1,6 +1,8 @@
 package com.zju.lease.web.admin.controller.system;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.zju.lease.common.result.Result;
 import com.zju.lease.model.entity.SystemUser;
@@ -54,18 +56,26 @@ public class SystemUserController {
     @Operation(summary = "判断后台用户名是否可用")
     @GetMapping("isUserNameAvailable")
     public Result<Boolean> isUsernameExists(@RequestParam String username) {
-        return Result.ok();
+        LambdaQueryWrapper<SystemUser> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(SystemUser::getUsername, username);
+        long count = service.count(queryWrapper);
+        return Result.ok(count == 0 );
     }
 
     @DeleteMapping("deleteById")
     @Operation(summary = "根据ID删除后台用户信息")
     public Result removeById(@RequestParam Long id) {
+        service.removeById(id);
         return Result.ok();
     }
 
     @Operation(summary = "根据ID修改后台用户状态")
     @PostMapping("updateStatusByUserId")
     public Result updateStatusByUserId(@RequestParam Long id, @RequestParam BaseStatus status) {
+        LambdaUpdateWrapper<SystemUser> updateWrapper = new LambdaUpdateWrapper<>();
+        updateWrapper.eq(SystemUser::getId, id);
+        updateWrapper.set(SystemUser::getStatus, status);
+        service.update(updateWrapper);
         return Result.ok();
     }
 }
