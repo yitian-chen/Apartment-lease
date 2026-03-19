ca1b23393d0c285efc9177099a036ff14ab16560
Chen <chenyitian01@zju.edu.cn>
2026-03-19 11:40:19 +0800

    fix: 修好了minio上传的broken pipe错误

--- common/src/main/java/com/zju/lease/common/minio/MinioConfiguration.java
+++ common/src/main/java/com/zju/lease/common/minio/MinioConfiguration.java
@@ -1,10 +1,13 @@
 package com.zju.lease.common.minio;
 
 import io.minio.MinioClient;
+import okhttp3.OkHttpClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.net.Proxy;
 
 @Configuration
 @ConfigurationPropertiesScan("com.zju.lease.common.minio")
@@ -13,11 +16,16 @@ public class MinioConfiguration {
     @Autowired
     private MinioProperties properties;
 
+    OkHttpClient httpClient = new OkHttpClient.Builder()
+            .proxy(Proxy.NO_PROXY)
+            .build();
+
     @Bean
     public MinioClient minioClient() {
         return MinioClient.builder()
                 .endpoint(properties.getEndpoint())
                 .credentials(properties.getAccessKey(), properties.getSecretKey())
+                .httpClient(httpClient)
                 .build();
     }
 }
