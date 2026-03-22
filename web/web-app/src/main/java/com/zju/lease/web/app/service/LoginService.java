18742538cb9ea226c8ccceaf34999b4aff7499cc
Chen <chenyitian01@zju.edu.cn>
2026-03-22 19:21:03 +0800

    完成用户登录/注册接口

--- web/web-app/src/main/java/com/zju/lease/web/app/service/LoginService.java
+++ web/web-app/src/main/java/com/zju/lease/web/app/service/LoginService.java
@@ -1,5 +1,9 @@
 package com.zju.lease.web.app.service;
 
+import com.zju.lease.web.app.vo.user.LoginVo;
+
 public interface LoginService {
     void getCode(String phone) throws Exception;
+
+    String login(LoginVo loginVo);
 }
