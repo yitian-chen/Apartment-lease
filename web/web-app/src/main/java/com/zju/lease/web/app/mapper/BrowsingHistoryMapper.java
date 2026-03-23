3b97f365d31f65fa00f4684fa3ca133ea90003c8
Chen <chenyitian01@zju.edu.cn>
2026-03-23 14:01:42 +0800

    完成分页查询浏览历史接口，用到嵌套查询和自定义mapper

--- web/web-app/src/main/java/com/zju/lease/web/app/mapper/BrowsingHistoryMapper.java
+++ web/web-app/src/main/java/com/zju/lease/web/app/mapper/BrowsingHistoryMapper.java
@@ -1,5 +1,6 @@
 package com.zju.lease.web.app.mapper;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.zju.lease.model.entity.BrowsingHistory;
 import com.zju.lease.web.app.vo.history.HistoryItemVo;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
@@ -13,6 +14,7 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 */
 public interface BrowsingHistoryMapper extends BaseMapper<BrowsingHistory> {
 
+    IPage<HistoryItemVo> pageItemByUserId(Page<HistoryItemVo> page, Long userId);
 }
 
 
