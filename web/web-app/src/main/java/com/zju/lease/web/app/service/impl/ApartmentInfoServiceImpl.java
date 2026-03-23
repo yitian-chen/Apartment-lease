38333fc54e2953e37a96e2e22fd983ec0abd543b
Chen <chenyitian01@zju.edu.cn>
2026-03-23 12:17:00 +0800

    完成了根据id查询房间详细信息。虽然繁琐(需要查询的字段很多)但是原理很简单，都写过

--- web/web-app/src/main/java/com/zju/lease/web/app/service/impl/ApartmentInfoServiceImpl.java
+++ web/web-app/src/main/java/com/zju/lease/web/app/service/impl/ApartmentInfoServiceImpl.java
@@ -1,10 +1,23 @@
 package com.zju.lease.web.app.service.impl;
 
 import com.zju.lease.model.entity.ApartmentInfo;
+import com.zju.lease.model.entity.LabelInfo;
+import com.zju.lease.model.enums.ItemType;
 import com.zju.lease.web.app.mapper.ApartmentInfoMapper;
+import com.zju.lease.web.app.mapper.GraphInfoMapper;
+import com.zju.lease.web.app.mapper.LabelInfoMapper;
+import com.zju.lease.web.app.mapper.RoomInfoMapper;
 import com.zju.lease.web.app.service.ApartmentInfoService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.zju.lease.web.app.service.RoomInfoService;
+import com.zju.lease.web.app.vo.apartment.ApartmentItemVo;
+import com.zju.lease.web.app.vo.graph.GraphVo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 /**
  * @author liubo
@@ -14,6 +27,37 @@ import org.springframework.stereotype.Service;
 @Service
 public class ApartmentInfoServiceImpl extends ServiceImpl<ApartmentInfoMapper, ApartmentInfo>
         implements ApartmentInfoService {
+
+    @Autowired
+    private ApartmentInfoMapper apartmentInfoMapper;
+
+    @Autowired
+    private LabelInfoMapper labelInfoMapper;
+
+    @Autowired
+    private GraphInfoMapper graphInfoMapper;
+
+    @Autowired
+    private RoomInfoMapper roomInfoMapper;
+
+    @Override
+    public ApartmentItemVo selectApartmentItemVoById(Long id) {
+        ApartmentInfo apartmentInfo = apartmentInfoMapper.selectById(id);
+
+        List<LabelInfo> labelInfoList = labelInfoMapper.selectListByApartmentId(id);
+
+        List<GraphVo> graphVoList = graphInfoMapper.selectListByItemTypeAndId(ItemType.APARTMENT, id);
+
+        BigDecimal minRent = roomInfoMapper.selectMinRentByApartmentId(id);
+
+        ApartmentItemVo apartmentItemVo = new ApartmentItemVo();
+        BeanUtils.copyProperties(apartmentInfo, apartmentItemVo);
+
+        apartmentItemVo.setGraphVoList(graphVoList);
+        apartmentItemVo.setLabelInfoList(labelInfoList);
+        apartmentItemVo.setMinRent(minRent);
+        return apartmentItemVo;
+    }
 }
 
 
