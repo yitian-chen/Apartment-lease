df29d1259c24f3ee92971529c2bc9743627792d5
Chen <chenyitian01@zju.edu.cn>
2026-03-24 09:45:12 +0800

    fix: 增加了枚举类converterFactory，修复了无法正确解析前端传来的枚举类的问题

--- web/web-app/src/main/java/com/zju/lease/web/app/custom/config/WebMvcConfiguration.java
+++ web/web-app/src/main/java/com/zju/lease/web/app/custom/config/WebMvcConfiguration.java
@@ -1,8 +1,10 @@
 package com.zju.lease.web.app.custom.config;
 
+import com.zju.lease.web.app.custom.converter.StringToBaseEnumConverterFactory;
 import com.zju.lease.web.app.custom.interceptor.AuthenticationInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,8 +14,16 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Autowired
     private AuthenticationInterceptor authenticationInterceptor;
 
+    @Autowired
+    private StringToBaseEnumConverterFactory stringToBaseEnumConverterFactory;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(this.authenticationInterceptor).addPathPatterns("/app/**").excludePathPatterns("/app/login/**");
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverterFactory(this.stringToBaseEnumConverterFactory);
     }
 }
